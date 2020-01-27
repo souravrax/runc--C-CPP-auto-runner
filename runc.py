b1dcc097010e65db1ac005bc678d4b3d089e5a78
Souravrax <rakshitsourav3@gmail.com>
2020-01-27 11:03:45 +0530

    Added config file

--- runc.py
+++ runc.py
@@ -3,6 +3,10 @@ import argparse
 import subprocess
 import os.path
 import time
+import json
+
+with open('./config.json') as f:
+    data = json.load(f)
 
 
 def open_file(file_path):
@@ -10,26 +14,33 @@ def open_file(file_path):
         time.sleep(1)
 
     if os.path.isfile(file_path):
+        print("Successfully compiled :-)\nOpening the file...")
+        print("=" * 70)
+        print()
         open_file = "./" + file_path
         subprocess.call([open_file])
     else:
         raise ValueError("%s isn't a file!" % file_path)
 
 
+parser = argparse.ArgumentParser(description=data["__description__"])
+# parser.add_argument("input", help="Name of the c++ file")
+parser.add_argument(
+    "-o", "--out", help="Specify the compiled file's name")
+parser.add_argument('-V','--version', action='version', version=data["__name__"] + " : " + data["__version__"])
 
-parser = argparse.ArgumentParser(description="Runs c++ files by reducing the compilation process")
-parser.add_argument("input", help="Name of the c++ file")
-parser.add_argument("-o", "--out", help="Specify the compiled file's name [Optional]")
-parser.add_argument("-u", "--upgrade", help="Upgrade the C++ compiler", default="11")
+
+# parser.add_argument()
+# parser.add_argument("-u", "--upgrade",help="Upgrade the C++ compiler", default="11")
 
 
 args = parser.parse_args()
 
-print(args.input)
-print(args.out)
+print("Compiling " + args.input + " ... ")
+time.sleep(0.5)
 if args.out:
     subprocess.call(["g++", str(args.input), "-o", str(args.out)])
     open_file((str(args.out) + ".exe"))
 else:
-    subprocess.call(["g++", args.input, args.upgrade])
+    subprocess.call(["g++", args.input])
     open_file("a.exe")
